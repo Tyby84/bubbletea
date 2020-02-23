c9e584f3c289fc6ee91d4973dc0d5c6ca84649ef
Tyby <tibofenyvesi84@gmail.com>
2020-02-23 16:14:26 +0100

    setup new

--- docs/src/views/Teak.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<div class="page">
-    <h3>Teak page</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur autem, debitis cumque expedita a consequuntur non eaque voluptas laudantium. Laborum voluptates ad id hic nobis laboriosam soluta excepturi perferendis eveniet.</p>
-</div>
-    
-</template>
