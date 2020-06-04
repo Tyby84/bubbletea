1a04067e5f99eeb6d8081633eb60eb11006feeb3
Tyby <tibofenyvesi84@gmail.com>
2020-06-04 17:05:19 +0200

    circle

--- src/views/Gyum_bubik.vue
+++ src/views/Gyum_bubik.vue
@@ -3,23 +3,41 @@
     <h3>Gyümölcs Buborékok</h3>
     <div class="aromak">
     <img class="aroma" src="/images/bubik/Áfonya GYÜMÖLCS  bubiv2.png" alt="Áfonya buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Ananász GYÜMÖLCS  bubiv2.png" alt="Ananász buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Banán GYÜMÖLCS  bubiv2.png" alt="Banán buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Barack GYÜMÖLCS  bubiv2.png" alt="Barack buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/csoki GYÜMÖLCS  bubiv2.png" alt="Csoki buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Gránátalma GYÜMÖLCS  bubiv2.png" alt="Gránátalma buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/kávé GYÜMÖLCS  bubiv2.png" alt="Kávé buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Kiwi GYÜMÖLCS  bubiv2.png" alt="Kiwi buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Licsi GYÜMÖLCS  bubiv2.png" alt="Licsi buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Málna GYÜMÖLCS  bubiv2.png" alt="Málna buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Mangó GYÜMÖLCS  bubiv2.png" alt="Mangó buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Meggy GYÜMÖLCS  bubiv2.png" alt="Meggy buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Narancs GYÜMÖLCS  bubiv2.png" alt="Narancs buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Sárgadinnye GYÜMÖLCS  bubiv2.png" alt="Sárgadinnye buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Szamóca GYÜMÖLCS  bubiv2.png" alt="Szamóca buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Szõlõ GYÜMÖLCS  bubiv2.png" alt="Szőlő buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Vörös Áfonya GYÜMÖLCS  bubiv2.png" alt="Vörös Áfonya buborék">
+    <span class="circle"></span>
     <img class="aroma" src="/images/bubik/Zöld alma GYÜMÖLCS  bubiv2.png" alt="Zöld alma buborék">
+    <span class="circle"></span>
     </div>
  </div>
 </template>
