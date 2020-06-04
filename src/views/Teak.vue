139c6ace8a82c5a84c34310acc82c8cc14abe8c5
Tyby <tibofenyvesi84@gmail.com>
2020-06-04 20:03:05 +0200

    pics

--- src/views/Teak.vue
+++ src/views/Teak.vue
@@ -3,7 +3,9 @@
     <h3>Teák</h3>
     <div class="aromak">
             <img class="aroma" src="/images/feketetea.png" alt="Fekete Tea">
+            <span class="circle"></span>
             <img class="aroma" src="/images/zoldtea.png" alt="Zöld Tea">
+            <span class="circle"></span>
     </div>
 </div>
     
--- src/views/Zselek.vue
+++ src/views/Zselek.vue
@@ -3,10 +3,15 @@
     <h3>Gyümölcs Zselék</h3>
     <div class="aromak">
       <img  class="aroma" src="images/zselek/Áfonya GYÜMÖLCS  zsele-01.png">
+      <span class="circle"></span>
       <img  class="aroma" src="images/zselek/Ananász GYÜMÖLCS  zsele-01.png">
+      <span class="circle"></span>
       <img  class="aroma" src="images/zselek/Licsi GYÜMÖLCS  zsele-01.png">
+      <span class="circle"></span>
       <img  class="aroma" src="images/zselek/Szamóca GYÜMÖLCS  zsele-01.png">
+      <span class="circle"></span>
       <img  class="aroma" src="images/zselek/Zold Alma GYÜMÖLCS  zsele-01.png">
+      <span class="circle"></span>
     </div>
     
     </div>
