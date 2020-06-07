0ef3d99bcc6ac450c9d849a802ce579959087531
Tyby <tibofenyvesi84@gmail.com>
2020-06-07 20:22:04 +0200

    soc ikon, fixes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="page">
     <h3 id="top">Rólunk</h3>
-    <p>Büszkén mondhatjuk el Nektek, hogy immáron két éve üzemel a <br> J Koktél Bubble Tea.</p>
-    <p>Kecskeméten elsőkézből ajánljuk a Tajvan ról származó különleges ízvilágot. <br> 
+    <p>Büszkén mondhatjuk el Nektek, hogy immáron két éve üzemel a <br> J Koktél Bubble Tea.</p><br>
+    <p>Kecskeméten elsőkézből ajánljuk a Tajvan-ról származó különleges ízvilágot. <br> 
      Tea választékunk frissen van főzve, ízesítése lehet Gyümölcsös vagy Tejes - amihez színes feltétet - 
      Gyümölcs Buborékot és vagy Gyümölcs Zselét választhatsz a tökéletes ízért!</p> <br>
-    <p>Megtiszteltetés, hogy a vásárlóink visszajelzése alapján Mi készítjük az ország legfinomabb Bubble Tea jét.</p><br>
+    <p>Megtiszteltetés, hogy a vásárlóink visszajelzése alapján Mi készítjük az ország legfinomabb Bubble Tea-jét.</p><br>
     <p>Nincs más hátra mint - válaszd ki a teád, ízesítsd és adj feltétet hozzá - még ma.</p>
     <a href="#top" class="totop" ><i class="fas fa-chevron-up"></i></a>
   </div>
