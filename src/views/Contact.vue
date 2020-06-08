f8a33d23ff5544232c376c5d608f38bed70eed3c
Tyby <tibofenyvesi84@gmail.com>
2020-06-08 22:19:05 +0200

    ds  d

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,5 +4,14 @@
     <img src="/images/contact.png" width="100%" alt="Contact">
     <a class="contact" target="_blank" href="https://www.google.com/maps/place//data=!4m2!3m1!1s0x4743db8da860d687:0xe6555099849f565b?utm_source=mstt_1">
     <h4><i class="fas fa-map-marker-alt"></i> Nézz be hozzánk!</h4></a>
+    <footer>
+     <div class="social_icons">
+     <a target="_blank" href="https://www.facebook.com/jkoktelbubbletea/"><img src="images/PNG/facebook.png" /></a>
+     </div>
+     <p>&copy; jkoktelbubbletea | 2020</p>
+     <div>
+     <a target="_blank" href="https://www.instagram.com/jkoktelbubbletea/"><img src="images/PNG/instagram.png" /></a>
+     </div>
+   </footer>
     </div>
 </template>
--- src/views/Gyum_bubik.vue
+++ src/views/Gyum_bubik.vue
@@ -41,5 +41,14 @@
     <span class="circle"></span>
     </div>
     <a href="#top" class="totop" ><i class="fas fa-chevron-up"></i></a>
+    <footer>
+     <div class="social_icons">
+     <a target="_blank" href="https://www.facebook.com/jkoktelbubbletea/"><img src="images/PNG/facebook.png" /></a>
+     </div>
+     <p>&copy; jkoktelbubbletea | 2020</p>
+     <div>
+     <a target="_blank" href="https://www.instagram.com/jkoktelbubbletea/"><img src="images/PNG/instagram.png" /></a>
+     </div>
+   </footer>
  </div>
 </template>
